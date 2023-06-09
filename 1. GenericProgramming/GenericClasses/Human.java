7b7d5ea09e4290975cc04aff8e7a4eaacf2ad053
USJ-Programming-Club <135755132+USJ-Programming-Club@users.noreply.github.com>
2023-06-09 14:09:17 +0300

    Delete 1. GenericProgramming directory

--- 1. GenericProgramming/GenericClasses/Human.java
+++
@@ -1,5 +0,0 @@
-package GenericClasses;
-
-interface Human {
-    void eat();
-}
--- 1. GenericProgramming/GenericClasses/Student.java
+++
@@ -1,8 +0,0 @@
-package GenericClasses;
-
-public class Student extends Person {
-    @Override
-    public void eat() {
-        System.out.println("Student eats");
-    }
-}
